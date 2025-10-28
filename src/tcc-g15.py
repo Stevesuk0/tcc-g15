a5e8726d9f57e9565be1d85a403b9d7ae55577b3
Stevesuk <stevesukawa@outlook.com>
2025-10-28 15:14:28 +0800

    Update tcc-g15.py

--- src/tcc-g15.py
+++ src/tcc-g15.py
@@ -1,5 +1,6 @@
 import sys
 import maliang
+import maliang.theme
 from PIL import Image
 from Backend import AWCCThermal, AWCCWmiWrapper, DetectHardware
 import threading
@@ -30,16 +31,51 @@ def main(minimize: bool = False):
     update_duration = 1000
 
     maliang.configs.Env.system = 'Windows10'
+    # maliang.theme.apply_theme(root, theme='acrylic')
+    maliang.theme.customize_window(root, disable_maximize_button=True)
 
     def update_info():
+        fan_ids = [id for id, _ in awccthermal._fanIdsAndRelatedSensorsIds]
+        cpu_fan_id = fan_ids[0]
+        gpu_fan_id = fan_ids[1]
+
         ui_gpu_temp.set(awccthermal.getFanRelatedTemp(hardware.GPUFanIdx) / 95)
         ui_gpu_temp_text.set(f'{awccthermal.getFanRelatedTemp(hardware.GPUFanIdx)} ℃')
-
-        ui_cpu_fan.set(f'{awccthermal.getFanRelatedTemp(hardware.GPUFanIdx)} ℃')
-        ui_gpu_fan_text.set(f'{awccthermal.getFanRPM(hardware.GPUFanIdx)} RPM')
+        ui_gpu_fan.set(awccthermal._awcc.GetFanRPM(gpu_fan_id) / 5500)
+        ui_gpu_fan_text.set(f'{awccthermal._awcc.GetFanRPM(gpu_fan_id)} RPM')
 
         ui_cpu_temp.set(awccthermal.getFanRelatedTemp(hardware.CPUFanIdx) / 110)
         ui_cpu_temp_text.set(f'{awccthermal.getFanRelatedTemp(hardware.CPUFanIdx)} ℃')
+        ui_cpu_fan.set(awccthermal._awcc.GetFanRPM(cpu_fan_id) / 5500)
+        ui_cpu_fan_text.set(f'{awccthermal._awcc.GetFanRPM(cpu_fan_id)} RPM')
+
+        ui_gpu_temp.style.set(
+            bg_bar  = ('#34A853', '#34A853'), 
+            ol_bar  = ('#34A853', '#34A853'), 
+            bg_slot = ('#505050', '#505050'), 
+            ol_slot = ('#808080', '#808080'),
+        )
+
+        ui_cpu_temp.style.set(
+            bg_bar  = ('#34A853', '#34A853'), 
+            ol_bar  = ('#34A853', '#34A853'), 
+            bg_slot = ('#505050', '#505050'), 
+            ol_slot = ('#808080', '#808080'),
+        )
+
+        ui_gpu_fan.style.set(
+            bg_bar  = ('#1A6497', '#1A6497'), 
+            ol_bar  = ('#1A6497', '#1A6497'), 
+            bg_slot = ('#505050', '#505050'), 
+            ol_slot = ('#808080', '#808080'),
+        )
+        
+        ui_cpu_fan.style.set(
+            bg_bar  = ('#1A6497', '#1A6497'), 
+            ol_bar  = ('#1A6497', '#1A6497'), 
+            bg_slot = ('#505050', '#505050'), 
+            ol_slot = ('#808080', '#808080'),
+        )
 
         root.after(update_duration, update_info)
 
@@ -56,8 +92,9 @@ def main(minimize: bool = False):
     ui_gpu_temp_text = maliang.Text(cv, position=(350, 55), fontsize=17, family='Microsoft YaHei UI')
     ui_gpu_fan = maliang.ProgressBar(cv, position=(25, 95), size=(310, 40))
     ui_gpu_fan_text = maliang.Text(cv, position=(350, 105), fontsize=17, family='Microsoft YaHei UI')
+    ui_gpu_fan_slider = maliang.Slider(cv, position=())
 
-    ui_cpu_prefix = size[0] // 2
+    ui_cpu_prefix = size[0] // 2 - 15
 
     cpu_name = hardware.getHardwareName(hardware.CPUFanIdx)
     ui_cpu_name = maliang.Text(cv, position=(ui_cpu_prefix + 25, 15), text=cpu_name, fontsize=17, family='Microsoft YaHei UI')    
@@ -65,7 +102,7 @@ def main(minimize: bool = False):
     ui_cpu_temp_text = maliang.Text(cv, position=(ui_cpu_prefix + 350, 55), fontsize=17, family='Microsoft YaHei UI')
     ui_cpu_fan = maliang.ProgressBar(cv, position=(ui_cpu_prefix + 25, 95), size=(310, 40))
     ui_cpu_fan_text = maliang.Text(cv, position=(ui_cpu_prefix + 350, 105), fontsize=17, family='Microsoft YaHei UI')
-    
+
     root.after(update_duration, update_info)
 
     root.mainloop()
