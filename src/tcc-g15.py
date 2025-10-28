6badd0e0c7d62bb40eb645ca0f878d6485318a6a
Stevesuk <stevesukawa@outlook.com>
2025-10-28 20:46:14 +0800

    ！

--- src/tcc-g15.py
+++ src/tcc-g15.py
@@ -6,6 +6,7 @@ from Backend import AWCCThermal, AWCCWmiWrapper, DetectHardware
 import threading
 import tempfile
 import os
+from pystray import Icon, MenuItem, Menu
 
 def create_lock():
     basename = (
@@ -19,7 +20,7 @@ def create_lock():
     os.open(lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR)
         
 def main(minimize: bool = False):
-    size = 900, 250
+    size = 900, 270
 
     root = maliang.Tk(size=size, position=(-1000, -1000))
     root.title('Lumine')
@@ -37,11 +38,19 @@ def main(minimize: bool = False):
 
     ui_family = 'Segoe UI'
 
+    failsafe = False
+    failsafe_cpu = 95
+    failsafe_gpu = 85
+    failsafe_activated = False
+
+    current_mode = 0
+
     maliang.configs.Env.system = 'Windows10'
     # maliang.theme.apply_theme(root, theme='acrylic')
     maliang.theme.customize_window(root, disable_maximize_button=True)
 
     def update_info():
+        nonlocal failsafe_activated
         gpu_fan_id = awccthermal._fanIdsAndRelatedSensorsIds[hardware.GPUFanIdx][0]
         cpu_fan_id = awccthermal._fanIdsAndRelatedSensorsIds[hardware.CPUFanIdx][0]
 
@@ -83,28 +92,55 @@ def main(minimize: bool = False):
             ol_slot = (color_barol, color_barol),
         )
 
+        if failsafe:
+            if awccthermal.getFanRelatedTemp(hardware.GPUFanIdx) > failsafe_gpu or awccthermal.getFanRelatedTemp(hardware.CPUFanIdx) > failsafe_cpu:
+                if failsafe_activated:
+                    ui_failsafe_status.style.set(bg=('#E06C75', '#E06C75'))
+                    awccthermal.setMode(awccthermal.Mode.G_Mode)
+                    ui_modeset.set(1)
+                else:
+                    ui_failsafe_status.style.set(bg=('#E5C07B', '#E5C07B'))
+                    failsafe_activated = True
+            else:
+                ui_failsafe_status.style.set(bg=('#98C379', '#98C379'))    
+                failsafe_activated = False
+        else:
+            ui_failsafe_status.style.set(bg=('#2B2B2B', '#2B2B2B'))
+
         root.after(update_duration, update_info)
 
-    def set_fan_gpu(type, _):
+    def set_fan(type, _):
         if type == 'normal':
             awccthermal.setMode(awccthermal.Mode.Custom)
 
-            speed = int(ui_gpu_fan_slider.get() * 100)
+            cpu_speed = int(ui_cpu_fan_slider.get() * 127)
+            gpu_speed = int(ui_gpu_fan_slider.get() * 127)
 
-            print(ui_gpu_fan_slider.get(), speed)
+            awccthermal.setFanSpeed(hardware.CPUFanIdx, cpu_speed)
+            awccthermal.setFanSpeed(hardware.GPUFanIdx, gpu_speed)
 
-            awccthermal.setFanSpeed(hardware.GPUFanIdx, speed)
+    def set_mode(value):
+        nonlocal current_mode
 
-            
-    def set_fan_cpu(type, value):
-        if type == 'normal':
-            awccthermal.setMode(awccthermal.Mode.Custom)
+        current_mode = value
 
-            speed = int(ui_cpu_fan_slider.get() * 100)
+        ui_gpu_fan_slider.disable(True)
+        ui_cpu_fan_slider.disable(True)
 
-            print(ui_cpu_fan_slider.get(), speed)
+        match value:
+            case 0:
+                awccthermal.setMode(awccthermal.Mode.Balanced)
+            case 1:
+                awccthermal.setMode(awccthermal.Mode.G_Mode)
+            case 2:
+                ui_gpu_fan_slider.disable(False)
+                ui_cpu_fan_slider.disable(False)
+                awccthermal.setMode(awccthermal.Mode.Custom)
 
-            awccthermal.setFanSpeed(hardware.CPUFanIdx, speed)
+    def toggle_failsafe(value):
+        nonlocal failsafe
+
+        failsafe = value
 
     cv = maliang.Canvas(root)
     cv.place(x=0, y=0, width=size[0], height=size[1])
@@ -131,14 +167,36 @@ def main(minimize: bool = False):
 
     maliang.configs.Env.system = 'Windows11'
     ui_gpu_fan_slider = maliang.Slider(cv, position=(25, 150), size=(310, 40), default=0.5)
-    ui_gpu_fan_slider.bind_on_update(set_fan_gpu)
+    ui_gpu_fan_slider.bind_on_update(set_fan)
     ui_gpu_fan_slitext = maliang.Text(cv, position=(349, 155), text='Fan Speed', fontsize=18, family=ui_family)
 
     ui_cpu_fan_slider = maliang.Slider(cv, position=(ui_cpu_prefix + 25, 150), size=(310, 40), default=0.5)
-    ui_cpu_fan_slider.bind_on_update(set_fan_cpu)
+    ui_cpu_fan_slider.bind_on_update(set_fan)
     ui_cpu_fan_slitext = maliang.Text(cv, position=(ui_cpu_prefix + 349, 155), text='Fan Speed', fontsize=18, family=ui_family)
 
+    ui_modeset = maliang.SegmentedButton(cv, position=(25, 205), text=("Balanced", "Turbo", "Custom"), fontsize=18, family=ui_family, command=set_mode, default=0)
+    set_mode(0)
+
+    for i in ui_modeset.children:
+        i.style.set(bg=('#2B2B2B', '#3C3C3C', '#323232', '#3C3C3C', '#3C3C3C', '#323232'), ol=('', '', '', '', '', ''))
+
+    ui_failsafe = maliang.ToggleButton(cv, position=(size[0] - 195, 205), size=(100, ui_modeset.size[1]), text=("Fail-Safe"), fontsize=18, family=ui_family, command=toggle_failsafe)
+
+    ui_failsafe_status = maliang.Label(cv, position=(size[0] - 85, 205), size=(50, ui_modeset.size[1]))
+
     root.after(update_duration, update_info)
+    root.protocol("WM_DELETE_WINDOW", root.withdraw)
+
+    def run_tray():
+        icon = Icon("TkTray", icon=Image.open('icons/app.png').resize((64, 64)), menu=Menu(
+            MenuItem(text='Lumine v1.0', action=lambda: ...),
+            Menu.SEPARATOR,
+            MenuItem('Show Window', root.deiconify, default=True),
+            MenuItem('Exit', root.destroy)
+        ))
+        icon.run()
+
+    threading.Thread(target=run_tray, daemon=True).start()
 
     root.mainloop()
 
@@ -149,7 +207,7 @@ def wrapper():
         create_lock()
     except:
         raise PermissionError("Another instance of this app is already running")
-    
+
     return main("--minimized" in sys.argv)
 
 if __name__ == "__main__":
